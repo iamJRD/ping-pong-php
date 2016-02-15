fa4eb84cf57b30332207e511ad67e2dc7c397117
eddie duro and jared beckler <e.jamesduro@gmail.com>
2016-02-15 11:09:17 -0800

    remove unneeded code

--- app/app.php
+++ app/app.php
@@ -1,12 +1,8 @@
 <?php
     require_once __DIR__.'/../vendor/autoload.php';
     require_once __DIR__.'/../src/PingPongGenerator.php';
-//     use Symfony\Component\Debug\Debug;
-// Debug::enable();
-
 
     $app = new Silex\Application();
-    // $app['debug'] = true;
     $app->register(new Silex\Provider\TwigServiceProvider, array(
         'twig.path' => __DIR__.'/../views'
     ));
--- src/PingPongGenerator.php
+++ src/PingPongGenerator.php
@@ -5,16 +5,6 @@
         {
             $results = array();
             for($i = 1; $i <= $input; $i++){
-                // $ping_pong_result = $new_PingPongGenerator->generatePingPongArray($input);
-                // if($input % 3 == 0 && $input % 5 == 0 ) {
-                //     return 'ping pong';
-                // } elseif($input % 3 == 0){
-                //     return 'ping';
-                // } elseif($input % 5 == 0) {
-                //     return 'pong';
-                // } else {
-                //     return $input;
-                // }
                 if($i % 3 == 0 && $i % 5 == 0 ) {
                     array_push($results, 'ping pong');
                 } elseif($i % 3 == 0){
@@ -26,7 +16,6 @@
                 }
             }
             return $results;
-
         }
     }
 ?>
