292a3c46850eef04ff8066a28cbe9518a50719d1
eddie duro and jared beckler <e.jamesduro@gmail.com>
2016-02-15 09:53:39 -0800

    add working spec for numbers divisible by 5

--- src/PingPongGenerator.php
+++ src/PingPongGenerator.php
@@ -1,12 +1,14 @@
 <?php
     class PingPongGenerator
     {
-      function generatePingPongArray($input)
-      {
-        if($input % 3 == 0){
-          return 'ping';
+        function generatePingPongArray($input)
+        {
+            if($input % 3 == 0){
+              return 'ping';
+            } elseif($input % 5 == 0) {
+              return 'pong';
+            }
         }
-      }
 
     }
 ?>
--- tests/PingPongGeneratorTest.php
+++ tests/PingPongGeneratorTest.php
@@ -5,15 +5,28 @@
   {
       function test_checkPing_true()
       {
-        // arrange
-        $test_PingPongGenerator = new PingPongGenerator;
-        $input = 3;
+          // arrange
+          $test_PingPongGenerator = new PingPongGenerator;
+          $input = 3;
 
-        // Act
-        $result = $test_PingPongGenerator->generatePingPongArray($input);
+          // Act
+          $result = $test_PingPongGenerator->generatePingPongArray($input);
 
-        // Assert
-        $this->assertEquals('ping', $result);
+          // Assert
+          $this->assertEquals('ping', $result);
+      }
+
+      function test_checkPong_true()
+      {
+          // Arrange
+          $test_PingPongGenerator = new PingPongGenerator;
+          $input = 10;
+
+          // Act
+          $result = $test_PingPongGenerator->generatePingPongArray($input);
+
+          // Assert
+          $this->assertEquals('pong', $result);
       }
 
   }
